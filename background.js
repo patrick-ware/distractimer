459aa7d5fd1a483d81f8aa0380734c98373482df
Patrick Ware <patrick.r.ware@gmail.com>
2021-06-24 12:24:08 -0700

    adding timer code to background.js

--- background.js
+++ background.js
@@ -1,6 +1,26 @@
+// example code
 let color = '#3aa757';
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+// timer code moved over from popup
+let sec = document.getElementById("minutes").value*60
+
+let timer = setInterval(function(){
+    let displayMinutes = Math.floor(sec/60);
+    // set seconds to always display two digits
+    let displaySeconds = sec%60
+    displaySeconds +=""
+    if(displaySeconds.length == 1 || displaySeconds==9) {
+      displaySeconds = "0" + displaySeconds;
+    }
+    let remainingTime= displayMinutes.toString() +":"+displaySeconds.toString();
+    document.getElementById('timeLeft').innerHTML= remainingTime
+    sec--;
+    if (sec < 0) {
+        clearInterval(timer);
+    }
+}, 1000);
